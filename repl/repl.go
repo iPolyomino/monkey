89b8ff2fe9f14607c6849828ae1bf93f484eb47b
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2019-04-07 22:29:22 +0900

    repl, bool

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/iPolyomino/monkey/evaluator"
 	"github.com/iPolyomino/monkey/lexer"
 	"github.com/iPolyomino/monkey/parser"
 )
@@ -31,8 +32,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
